6eeb42f0f6b191a9f5c5ac461e2ef472c6df0bbc
Christina <c_j_y@ymail.com>
2015-10-23 09:19:02 -0500

    added javascript!

--- songs.js
+++ songs.js
@@ -1,29 +1,87 @@
-var songs = ["Happy", "Photograph", "Afire Love", "Flowers", "One Hell of an Amen"];
-
-songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
-songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-songs[songs.length] = "When the Sand Runs Out by Rascal Flatts";
+var songs = [];
+var mySongs = ""
 
 
-var phrase = "Legs > by Z*ZTop on the album Eliminator";
-var newPhrase = phrase.replace(">", "-");
-console.log(newPhrase);
+songs.push("Legs > by Z*ZTop on the album Eliminator", "The Logical Song > by Supertr@amp on the album Breakfast in America", "Another Brick in the Wall > by Pink Floyd on the album The Wall", "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction", "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill", "When the Sand Runs Out > by Rascal Flatts");
 
-var phrase = "The Logical Song > by Supertr@amp on the album Breakfast in America";
-var newPhrase = phrase.replace(">", "-");
-console.log(newPhrase);
 
-var phrase = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
-var newPhrase = phrase.replace(">", "-");
-console.log(newPhrase);
+songs.unshift("Photograph - by Ed Sheeran on the album X(Deluxe Edition");
+songs.push("Like I'm Gonna Lose You - by Meghan Trainor on the album Title");
+console.log(songs);
 
-var phrase = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
-var newPhrase = phrase.replace(">", "-");
-console.log(newPhrase);
 
-var phrase = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
-var newPhrase = phrase.replace(">", "-");
-console.log(newPhrase);
+for (var i = 0; i < songs.length; i++) {
+	console.log(songs[i]);
+	songs[i] = songs[i].replace(">", "-");
+	var changedSongs = songs[i].replace("*", "").replace("!", "").replace("@", "").replace("(", "");
+	changedSongs.split("");
+	console.log(changedSongs);
+
+	mySongs += "<div class='theseSongs'>" + changedSongs + "</div>";
+	console.log(mySongs);
+
+};
+
+	var songBox = document.getElementById("song-list");
+	songBox.innerHTML = mySongs;
+	console.log(songBox.innerHTML);
+
+
+var hiddenAdd = document.getElementById("addMusic");
+var hiddenList = document.getElementById("listMusic")
+
+hiddenAdd.style.display = 'none';
+hiddenList.style.display = 'block';
+
+
+document.getElementById("addLink").addEventListener("click", function() {
+	hiddenAdd.style.display = 'block';
+	hiddenList.style.display = 'none';
+});
+
+document.getElementById("listLink").addEventListener("click", function() {
+	hiddenAdd.style.display = 'none';
+	hiddenList.style.display = 'block';
+});
+
+
+var song = document.getElementById("nameField");
+var artist = document.getElementById("artistField");
+var album = document.getElementById("albumField");
+// var everything = document.getElementById("songs");
+// var addBtn = 
+
+document.getElementById("addButton").addEventListener("click", function addButton () {
+	var songName = song.value;
+	var artistName = artist.value;
+	var albumName = album.value;
+	printMessage(songBox);
+
+
+	function printMessage (songBox) {
+ 	songBox.innerHTML = "<div class='newSong'>" + songName + " - by " +
+ 	artistName + " on the album " + albumName + "</div>" + songBox.innerHTML;
+		
+	}
+
+
+  	clearInput();
+
+  	function clearInput() {
+  		song.value = "";
+  		artist.value = "";
+  		album.value = "";
+  	} 
+});
+
+console.log(songBox);
+
+
+
+
+
+
+
+
+
+
